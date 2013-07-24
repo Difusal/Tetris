a999eb8fc00a76081d342c54d73620dfc942ef55
Henrique Ferrolho <henriqueferrolho@gmail.com>
2013-07-24 23:12:00 +0100

    working on about state

--- Tetris/Tetris/AboutState.cpp
+++ Tetris/Tetris/AboutState.cpp
@@ -1,7 +1,6 @@
 #include "AboutState.h"
 
-void AboutState::Initialize()
-{
+void AboutState::Initialize() {
 	/* loading background */
 	background = al_load_bitmap(MainMenuAlternativeBackground);
 	if (!background) {
@@ -10,7 +9,13 @@ void AboutState::Initialize()
 	}
 
 	/* initializing variables */
-
+	text.push_back("Tetris v1.0");
+	text.push_back("Date: July, 2013");
+	text.push_back("Author: Henrique Ferrolho");
+	text.push_back("");
+	text.push_back("This game was made for educational purposes only.");
+	text.push_back("It was programmed using C++ language.");
+	text.push_back("Allegro 5 2D graphical library was used as well.");
 
 	/* pushing buttons */
 	screenTitle = new MenuButton("ABOUT", true);
@@ -39,8 +44,21 @@ void AboutState::Draw() {
 	/* drawing background */
 	al_draw_bitmap(background, 0, 0, NULL);
 
-	/* printing text with info about the game */
+	/* defining some variables... */
+	int spaceBetweenLines = 30;
+	int boxYMargin = 15;
+	unsigned int alpha = 96;
+	int textYPos = 80+(mainMenuButton->getYPos()-80)/2 - text.size()*spaceBetweenLines/2;
 
+	/* drawing text dark transparent background */
+	int y = textYPos + text.size()*spaceBetweenLines;
+	al_draw_filled_rectangle(0, textYPos-3-boxYMargin, ScreenWidth, y+boxYMargin, al_map_rgba(32*alpha, 32*alpha, 32*alpha, alpha));
+
+	/* printing text with info about the game */
+	for (unsigned int i = 0; i < text.size(); i++) {
+		al_draw_text(Tetris::GetInstance()->regular_font, Black, 20+1, textYPos + i*spaceBetweenLines + 2, ALLEGRO_ALIGN_LEFT, text[i]);
+		al_draw_text(Tetris::GetInstance()->regular_font, White, 20, textYPos + i*spaceBetweenLines, ALLEGRO_ALIGN_LEFT, text[i]);
+	}
 
 	/* checking if any button is being hovered */
 	for (unsigned int i = 0; i < buttons.size(); i++)
@@ -50,6 +68,9 @@ void AboutState::Draw() {
 void AboutState::Terminate() {
 	/* destroying background bitmap */
 	al_destroy_bitmap(background);
+
+	/* clearing text */
+	text.clear();
 
 	/* deleting buttons */
 	for (unsigned int i = 0; i < buttons.size(); i++)
